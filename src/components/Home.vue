5e08d7288cad3299ba865c3c2fad5debc6ebaa12
Couch <marius-h@users.noreply.github.com>
2018-04-17 16:15:54 +0200

    Commit für den Laurin

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,11 +7,15 @@
     >
       <v-carousel-item
         v-for="(item, i) in items"
+        :src="'/static/screenshotApp1.png'"
         :key="i"
       >
-          <img
+        <div class="title">
+          {{ item.title }}
+        </div>
+          <!--img
             :src="'../static/screenshotApp1.png'"
-            style="-webkit-filter: blur(3px)" width="2000"/>
+            style="-webkit-filter: blur(3px)" width="2000"/-->
       </v-carousel-item>
     </v-carousel>
   </section>
@@ -26,10 +30,22 @@
       return {
         items: [
           {
-            src: '../static/screenshotApp1.png'
+            src: '../static/screenshotApp1.png',
+            title: 'Elternsprechtagplaner'
           }
         ]
       }
     }
   }
 </script>
+
+<style scoped>
+  .title {
+    position: absolute;
+    bottom: 50px;
+    background-color: rgba(0, 0, 0, 0.5);
+    color: WHITE;
+    font-size: 2em;
+    padding: 20px;
+  }
+</style>
