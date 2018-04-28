e4f21acd78367aa342a2874a6a5aa2553c86419d
Laurin Dörre <laurin.doerre@lgs-hu.eu>
2018-04-28 22:06:37 +0200

    Sprache und Carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -12,13 +12,26 @@
       v-for="(item,i) in items"
       :key="i"
     >
+      <v-container grid-list-md text-xs-center>
       <v-layout
-        column
+        row
         wrap
         align-center
       >
-        <img :src="item.src" style="max-height: 500px"/>
+
+        <v-flex xs4 md5>
+        <div class="headline">John <span style="font-weight:bold">Carter</span></div>
+          <div>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</div>
+        </v-flex>
+
+        <v-flex xs4 md5>
+        <img :src="item.src" style="max-height: 500px; min-height: 500px"/>
+        </v-flex>
+
+
+
       </v-layout>
+      </v-container>
     </v-carousel-item>
     </v-parallax>
   </v-carousel>
